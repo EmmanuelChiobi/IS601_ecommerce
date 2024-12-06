83cb8f28a908181dbc261261797381be62e484c5
Emmanuel Chiobi <ec362@njit.edu>
2024-12-05 20:26:20 -0500

    Continuing on authentication

--- ecommerce/auth/models.py
+++ ecommerce/auth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+# from products.models import Product
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    # cart = models.ManyToManyField(Product)
