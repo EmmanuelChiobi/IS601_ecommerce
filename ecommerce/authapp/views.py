2ed478374a469560c609d68016c30700a5b979e4
Emmanuel Chiobi <ec362@njit.edu>
2024-12-07 14:25:10 -0500

    Adding forms to login and register

--- ecommerce/authapp/views.py
+++ ecommerce/authapp/views.py
@@ -4,4 +4,7 @@ from django.contrib import messages
 
 # Create your views here.
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    return render(request, 'templates/auth/login.html', {})
+
+def register_user(request):
+    return render(request, 'templates/auth/register.html', {})
