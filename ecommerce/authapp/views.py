0acfb450fcdbfbcf2901bb931374d57b7d06a7cf
Emmanuel Chiobi <ec362@njit.edu>
2024-12-09 21:15:42 -0500

    Continuing work on auth

--- ecommerce/authapp/views.py
+++ ecommerce/authapp/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def login_user(request):
     return render(request, 'auth/login.html', {})
 
 def register_user(request):
-    return render(request, 'auth/register.html', {})
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'auth/register.html', {'form': form})
